30d7813649059fe953ba03e7c512aa8012856b0d
Sergei Vas <byserge@gmail.com>
2024-04-20 15:03:32 -0400

    navbar

--- app/components/global/Navbar.tsx
+++ app/components/global/Navbar.tsx
@@ -26,43 +26,45 @@ export default function Navbar() {
       href: "/photos",
     },
   ];
-  const SCROLL_UP_THRESHOLD = 6;
+  const SCROLL_UP_THRESHOLD = 5;
   const navbarRef = useRef<HTMLElement>(null);
 
+  const [isTop, setIsTop] = useState(true); // состояние для отслеживания нахождения в верху страницы
   const [isVisible, setIsVisible] = useState(true);
   const lastScrollY = useRef(0);
   const [navbarHeight, setNavbarHeight] = useState(0);
 
   const handleScroll = () => {
     const currentScrollY = window.scrollY;
-    // Only set state if scroll position changes to avoid unnecessary re-renders
+    // Определите, находится ли пользователь в самом верху страницы
+    setIsTop(currentScrollY === 0);
+
     if (currentScrollY > lastScrollY.current && currentScrollY > navbarHeight) {
-      isVisible && setIsVisible(false);
-    } else if (lastScrollY.current - currentScrollY > SCROLL_UP_THRESHOLD) { // Only show the navbar if scrolled up more than 100px
-      !isVisible && setIsVisible(true);
+        isVisible && setIsVisible(false);
+    } else if (lastScrollY.current - currentScrollY > SCROLL_UP_THRESHOLD) {
+        !isVisible && setIsVisible(true);
     }
     lastScrollY.current = currentScrollY;
-  };
+};
 
   useEffect(() => {
     setNavbarHeight(navbarRef.current?.offsetHeight || 0);
     window.addEventListener('scroll', handleScroll);
-  
-    // Clean up event listener to prevent memory leaks
+
     return () => window.removeEventListener('scroll', handleScroll);
-  }, [isVisible, navbarHeight]); // Only re-run effect if isVisible 
-  
+  }, [isVisible, navbarHeight, isTop]); // Only re-run effect if isVisible 
+    
 
   return (
     <UnmountStudio>
-      <header ref={navbarRef} className={`text-sm py-6 md:px-16 px-6 border-b dark:border-zinc-800 border-zinc-200 z-30 md:mb-28 mb-10 sticky top-0 shadow-sm transition-transform duration-500 ${isVisible  ? 'translate-y-0' : '-translate-y-full'}`}>
+      <header ref={navbarRef} className={`text-sm py-6 md:px-16 px-6 border-b dark:border-zinc-800 border-zinc-200 z-30 md:mb-28 mb-10 sticky top-0 transition-transform duration-500 shadow-sm ${isVisible ? 'translate-y-0' : '-translate-y-full'} ${isTop ? 'bg-transparent' : 'bg-[#ebebeb]/70 dark:bg-zinc-900/70'}`}>
         <div className="max-w-6xl mx-auto flex items-center justify-between">
           <Link href="/">
             <Image src={Logo} width={35} height={35} alt="logo" priority />
           </Link>
 
-          <nav className="md:block hidden">
-            <ul className="flex items-center gap-x-8">
+          <nav className="md:flex hidden flex-1 justify-end ">
+            <ul className="flex items-center gap-x-8 mr-12">
               {data.map((link, id) => (
                 <li key={id}>
                   <Link
