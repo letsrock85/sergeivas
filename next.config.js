7b05c2cab80dfda6f6b0675c1e9a86b8e25c0c39
Sergei Vas <byserge@gmail.com>
2024-12-04 17:04:54 -0500

    fixes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
     // Отключаем проверку ESLint при сборке на Vercel
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+      },
+      { hostname: "icons.duckduckgo.com" },
+      { hostname: "res.cloudinary.com" },
+      { hostname: "www.google.com" },
+      { hostname: "images.unsplash.com" },
+    ],
+  },
 }
 
 module.exports = nextConfig
